d1fa5871bafdac3d41c0fd8aa59ad175c519704e
Vinicius Sueiro <hi@vsueiro.com>
2023-03-21 09:06:19 -0400

    Try to fix media preloading

--- scripts/script.js
+++ scripts/script.js
@@ -46,21 +46,21 @@ form.age.addEventListener(
 form.ageDisplay.update();
 
 new Preloader(
-  "../../media/candle-number-9.png",
-  "../../media/wick-1.png",
-  "../../media/cake-outline.png",
-  "../../media/cake.png",
-  "../../media/candle-number-0.png",
-  "../../media/candle-number-1.png",
-  "../../media/candle-number-2.png",
-  "../../media/candle-number-3.png",
-  "../../media/candle-number-4.png",
-  "../../media/candle-number-5.png",
-  "../../media/candle-number-6.png",
-  "../../media/candle-number-7.png",
-  "../../media/candle-number-8.png",
-  "../../media/candle-number-9.png",
-  "../../media/stick-0.png",
-  "../../media/stick-1.png",
-  "../../media/wick-0.png"
+  "../media/candle-number-9.png",
+  "../media/wick-1.png",
+  "../media/cake-outline.png",
+  "../media/cake.png",
+  "../media/candle-number-0.png",
+  "../media/candle-number-1.png",
+  "../media/candle-number-2.png",
+  "../media/candle-number-3.png",
+  "../media/candle-number-4.png",
+  "../media/candle-number-5.png",
+  "../media/candle-number-6.png",
+  "../media/candle-number-7.png",
+  "../media/candle-number-8.png",
+  "../media/candle-number-9.png",
+  "../media/stick-0.png",
+  "../media/stick-1.png",
+  "../media/wick-0.png"
 );
