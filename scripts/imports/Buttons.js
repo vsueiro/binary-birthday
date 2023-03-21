58f1d0faf70895d79eff48407726f549cec864fc
Vinicius Sueiro <hi@vsueiro.com>
2023-03-21 09:27:13 -0400

    Use click event once to enable audio play

--- scripts/imports/Buttons.js
+++ scripts/imports/Buttons.js
@@ -8,6 +8,7 @@ export default class Buttons {
     this.delay = 300;
     this.debounce = 150;
     this.isPressing = false;
+    this.clicked = false;
 
     this.enableActiveState();
     this.updateAgeOnPress();
@@ -25,9 +26,14 @@ export default class Buttons {
     }
   }
 
-  handlePress(step) {
+  handlePress(step, origin) {
     // Make first increment
     this.form.constrain("age", undefined, step);
+
+    // Prevent fast increment if origin was first user click
+    if (origin === "click") {
+      return;
+    }
 
     // Wait before triggerring fast increment mode
     this.timeout = setTimeout(() => {
@@ -65,25 +71,45 @@ export default class Buttons {
       // Get step (like -1 or +1)
       const step = Number(button.dataset.step);
 
+      // When the first click occurs
+      button.addEventListener(
+        "click",
+        () => {
+          // Prevent increment if one of the buttons was already clicked
+          if (!this.clicked) {
+            this.handlePress(step, "click");
+
+            // Set flag to prevent further events from firing
+            this.clicked = true;
+          }
+        },
+        { once: true }
+      );
+
       // When touch begins
       button.addEventListener("touchstart", (event) => {
-        this.handlePress(step);
-
-        // Prevent further mouse events from firing
-        event.preventDefault();
+        // If a click was fired once (to enable audio)
+        if (this.clicked) {
+          this.handlePress(step);
+          // Prevent further mouse events from firing
+          event.preventDefault();
+        }
       });
 
       // When touch ends
       button.addEventListener("touchend", (event) => {
         this.handleRelease();
-
         // Prevent further mouse events from firing
         event.preventDefault();
+        // }
       });
 
       // When mousedown begins
       button.addEventListener("mousedown", () => {
-        this.handlePress(step);
+        // If a click was fired once (to enable audio)
+        if (this.clicked) {
+          this.handlePress(step);
+        }
       });
 
       // When mouseup is fired
