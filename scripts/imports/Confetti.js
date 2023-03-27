e8f5b6d8b691beb7ad57dbea4e32c70b37e38ccc
Vinicius Sueiro <hi@vsueiro.com>
2023-03-27 18:08:18 -0400

    Start with a blank sheet

--- scripts/imports/Confetti.js
+++ scripts/imports/Confetti.js
@@ -8,27 +8,29 @@ export default class Confetti {
   }
 
   randomSymbol() {
-    let index = Math.floor(Math.random() * this.symbols.length);
+    const index = Math.floor(Math.random() * this.symbols.length);
     return this.symbols[index];
   }
 
   randomDelay() {
-    // -20 to 20
+    // -20 to 20s
     return Math.random() * 40 - 20;
   }
 
   randomDuration() {
-    // 8 to 16
+    // 8 to 16s
     return 8 + Math.random() * 8;
   }
 
   create() {
+    let markup = ``;
+
     for (let i = 0; i < this.amount; i++) {
       const symbol = this.randomSymbol();
       const delay = this.randomDelay();
       const duration = this.randomDuration();
 
-      const markup = `
+      markup += `
         <div
           class="symbol-track"
           style="animation-delay: ${delay}s; animation-duration: ${duration}s">
@@ -39,7 +41,8 @@ export default class Confetti {
           </div>
         </div>
       `;
-      this.element.insertAdjacentHTML("beforeend", markup);
     }
+
+    this.element.insertAdjacentHTML("beforeend", markup);
   }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,7 @@
 import Preloader from "./imports/Preloader.js";
 import Confetti from "./imports/Confetti.js";
-import Candles from "./imports/Candles.js";
-import Form from "./imports/Form.js";
-import Buttons from "./imports/Buttons.js";
 
-const form = new Form("form");
-
-new Buttons("button", form);
 new Confetti(".confetti");
-
-// When form is submitted
-form.callback((form) => {
-  // Get object with form values
-  const data = form.data;
-
-  // Get age value
-  const age = data.age;
-
-  // Convert age from decimal to binary
-  const candles = new Candles(age, ".candles");
-
-  // Show result descriptioon
-  candles.describe(".description");
-
-  // Show candles
-  candles.show();
-});
-
-// When age is manually changed
-form.age.addEventListener("input", () => {
-  const value = Number(form.age.value);
-  form.constrain("age", value);
-});
-
-// Clear age input the first time it is focused
-form.age.addEventListener(
-  "focus",
-  () => {
-    form.constrain("age", 0);
-    return;
-  },
-  { once: true }
-);
-
-form.ageDisplay.update();
 
 new Preloader(
   "./media/candle-number-9.png",
