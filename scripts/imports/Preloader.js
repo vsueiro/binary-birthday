4d2f930f537249a0f5f136f4ec822a70773dd96e
Vinicius Sueiro <hi@vsueiro.com>
2023-04-23 11:41:18 -0400

    Add basic scripts inside App class

--- scripts/imports/Preloader.js
+++ scripts/imports/Preloader.js
@@ -1,8 +1,13 @@
 export default class Preloader {
   constructor(...images) {
+    this.length = images.length;
+    this.loaded = 0;
     for (let image of images) {
       const img = new Image();
       img.src = image;
+      img.onload = () => {
+        this.loaded++;
+      };
     }
   }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,3 @@
-import Preloader from "./imports/Preloader.js";
-import Confetti from "./imports/Confetti.js";
-import BirthdayCake from "./imports/BirthdayCake.js";
-import Explainer from "./imports/Explainer.js";
+import App from "./imports/App.js";
 
-const confetti = new Confetti(".confetti");
-const birthdayCake = new BirthdayCake(".birthday-cake");
-const explainer = new Explainer(".explainer", birthdayCake);
-
-new Preloader(
-  "./images/composite/logo.png",
-  "./images/button-minus.png",
-  "./images/button-plus.png",
-  "./images/cake.png",
-  "./images/cake-outline.png",
-  "./images/candle-number-0.png",
-  "./images/candle-number-1.png",
-  "./images/candle-number-2.png",
-  "./images/candle-number-3.png",
-  "./images/candle-number-4.png",
-  "./images/candle-number-5.png",
-  "./images/candle-number-6.png",
-  "./images/candle-number-7.png",
-  "./images/candle-number-8.png",
-  "./images/candle-number-9.png",
-  "./images/candle-stick.png",
-  "./images/candle-wick-0.png",
-  "./images/candle-wick-1.png",
-  "./images/strawberry-0.png",
-  "./images/strawberry-1-outline.png",
-  "./images/strawberry-1.png",
-  "./images/strawberry-2-outline.png",
-  "./images/strawberry-2.png",
-  "./images/strawberry-0-outline.png"
-);
+new App();
