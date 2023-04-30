99610e6f9060a925a1266af81382a2e161c7a741
Vinicius Sueiro <hi@vsueiro.com>
2023-04-30 16:26:53 -0400

    Reorganize App and Preloader classes

--- scripts/imports/App.js
+++ scripts/imports/App.js
@@ -6,36 +6,20 @@ import Sounds from "./Sounds.js";
 
 export default class App {
   constructor() {
+    this.delay = 1000;
+
+    this.setup();
+  }
+
+  setup() {
     window.sounds = new Sounds();
 
     this.confetti = new Confetti(".confetti");
-    this.birthdayCake = new BirthdayCake(".birthday-cake");
-    this.explainer = new Explainer(".explainer", this.birthdayCake);
-    this.preloader = new Preloader(
-      "./images/composite/logo.png",
-      "./images/button-minus.png",
-      "./images/button-plus.png",
-      "./images/cake.png",
-      "./images/cake-outline.png",
-      "./images/candle-number-0.png",
-      "./images/candle-number-1.png",
-      "./images/candle-number-2.png",
-      "./images/candle-number-3.png",
-      "./images/candle-number-4.png",
-      "./images/candle-number-5.png",
-      "./images/candle-number-6.png",
-      "./images/candle-number-7.png",
-      "./images/candle-number-8.png",
-      "./images/candle-number-9.png",
-      "./images/candle-stick.png",
-      "./images/candle-wick-0.png",
-      "./images/candle-wick-1.png",
-      "./images/strawberry-0.png",
-      "./images/strawberry-1-outline.png",
-      "./images/strawberry-1.png",
-      "./images/strawberry-2-outline.png",
-      "./images/strawberry-2.png",
-      "./images/strawberry-0-outline.png"
-    );
+    this.preloader = new Preloader();
+
+    setTimeout(() => {
+      this.birthdayCake = new BirthdayCake(".birthday-cake");
+      this.explainer = new Explainer(".explainer", this.birthdayCake);
+    }, this.delay);
   }
 }
--- scripts/imports/Preloader.js
+++ scripts/imports/Preloader.js
@@ -1,13 +1,39 @@
 export default class Preloader {
-  constructor(...images) {
-    this.length = images.length;
-    this.loaded = 0;
-    for (let image of images) {
+  constructor() {
+    this.images = [
+      "./images/composite/logo.png",
+      "./images/button-minus.png",
+      "./images/button-plus.png",
+      "./images/cake.png",
+      "./images/cake-outline.png",
+      "./images/candle-number-0.png",
+      "./images/candle-number-1.png",
+      "./images/candle-number-2.png",
+      "./images/candle-number-3.png",
+      "./images/candle-number-4.png",
+      "./images/candle-number-5.png",
+      "./images/candle-number-6.png",
+      "./images/candle-number-7.png",
+      "./images/candle-number-8.png",
+      "./images/candle-number-9.png",
+      "./images/candle-stick.png",
+      "./images/candle-wick-0.png",
+      "./images/candle-wick-1.png",
+      "./images/strawberry-0.png",
+      "./images/strawberry-1-outline.png",
+      "./images/strawberry-1.png",
+      "./images/strawberry-2-outline.png",
+      "./images/strawberry-2.png",
+      "./images/strawberry-0-outline.png",
+    ];
+
+    this.load();
+  }
+
+  load() {
+    for (let image of this.images) {
       const img = new Image();
       img.src = image;
-      img.onload = () => {
-        this.loaded++;
-      };
     }
   }
 }
